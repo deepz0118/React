4aba7192b03a0bf43d87a052f45ddc95dfc08a0b
Deepika P <thangamdeepi49@gmail.com>
2024-09-24 17:34:22 -0700

    Update api.js

--- sample2/src/services/api.js
+++ sample2/src/services/api.js
@@ -1,1 +1,11 @@
-import { Plus } from "lucide-react";
+import axios from 'axios'
+
+
+const API = "https://66e526f85cc7f9b6273c6cae.mockapi.io/user"
+
+const getProjects = () => axios.get(`${API}/projects`)
+const getProjectbyID = (id) => axios.get(`${API}/projects/${id}`)
+const addProject = (projectdata) => axios.post(`${API}/projects`, projectdata)
+const editProject = (id, projectdata) => axios.put(`${API}/projects/${id}`, projectdata)
+const deleteProject = (id) => axios.delete(`${API}/projects/${id}`)
+export { getProjects, getProjectbyID, addProject, editProject, deleteProject }
