e762ed2395b260a31bb0d3ee88e28fc23d17e4e5
Deepika P <thangamdeepi49@gmail.com>
2024-09-30 18:56:44 -0700

    Update server.js

--- Stack/Server/server.js
+++ Stack/Server/server.js
@@ -4,7 +4,7 @@ const dbConn=require('./config/db.js')
 require('dotenv').config()
 const port=process.env.PORT || 7778
 app.get('/',(req,res)=>{
-    res.json({message:"Welcome"})
+    res.json({message:"Welcome deepika"})
 })
 app.listen(port,()=>{
     console.log(`Server running in : ${port}`)
