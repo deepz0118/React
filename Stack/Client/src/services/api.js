e582487ebec580f12185b9d3993c101c8a7d289d
Deepika P <thangamdeepi49@gmail.com>
2024-10-06 14:08:28 -0700

    edit

--- Stack/Client/src/services/api.js
+++ Stack/Client/src/services/api.js
@@ -1,8 +1,5 @@
 import axios from 'axios'
-
-
 const API = "https://66e526f85cc7f9b6273c6cae.mockapi.io"
-
 const getProjects = () => axios.get(`${API}/user`)
 const getProjectbyID = (id) => axios.get(`${API}/user/${id}`)
 const addProject = (projectdata) => axios.post(`${API}/user`, projectdata)
--- Stack/Server/server.js
+++ Stack/Server/server.js
@@ -1,7 +1,7 @@
+require('dotenv').config()
 const express=require('express')
 const app=express()
 const dbConn=require('./config/db.js')
-require('dotenv').config()
 const port=process.env.PORT || 7778
 app.get('/',(req,res)=>{
     res.json({message:"Welcome deepika"})
